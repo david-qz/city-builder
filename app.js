42c1d5d31112551ba9f73b2588e8fa18873db758
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-06-09 14:45:06 -0700

    Collapse the names a bit
    I KNOW IT'S A CITY BUILDING APP ALREADY!

--- app.js
+++ app.js
@@ -2,72 +2,72 @@
 const city = {
     name: 'Pleasantville',
     biome: 'temperate',
-    architecture: 'victorian',
+    arch: 'victorian',
     slogans: [],
 };
 const cities = [];
 
 // City Designer Component
-const cityDesignerSection = document.querySelector('#designer-section');
-const cityDesignerNameInput = cityDesignerSection.querySelector('input');
-const [cityDesignerBiomeSelect, cityDesignerArchSelect] = cityDesignerSection.querySelectorAll('select');
-const cityDesignerSloganTextArea = cityDesignerSection.querySelector('textarea');
-const [cityDesignerSloganButton, cityDesignerSaveButton] = cityDesignerSection.querySelectorAll('button');
+const builderSection = document.querySelector('#builder-section');
+const builderNameInput = builderSection.querySelector('input');
+const [builderBiomeSelect, builderArchSelect] = builderSection.querySelectorAll('select');
+const builderSloganTextArea = builderSection.querySelector('textarea');
+const [builderSloganButton, builderSaveButton] = builderSection.querySelectorAll('button');
 
 function displayInput() {
-    cityDesignerNameInput.value = city.name;
-    cityDesignerBiomeSelect.value = city.biome;
-    cityDesignerArchSelect.value = city.architecture;
+    builderNameInput.value = city.name;
+    builderBiomeSelect.value = city.biome;
+    builderArchSelect.value = city.arch;
 }
 
-cityDesignerNameInput.addEventListener('input', () => {
-    city.name = cityDesignerNameInput.value;
+builderNameInput.addEventListener('input', () => {
+    city.name = builderNameInput.value;
     displayCity();
 });
 
-cityDesignerBiomeSelect.addEventListener('change', () => {
-    city.biome = cityDesignerBiomeSelect.value;
+builderBiomeSelect.addEventListener('change', () => {
+    city.biome = builderBiomeSelect.value;
     displayCity();
 });
 
-cityDesignerArchSelect.addEventListener('change', () => {
-    city.architecture = cityDesignerArchSelect.value;
+builderArchSelect.addEventListener('change', () => {
+    city.arch = builderArchSelect.value;
     displayCity();
 });
 
-cityDesignerSloganButton.addEventListener('click', () => {
-    const slogan = cityDesignerSloganTextArea.value.trim();
+builderSloganButton.addEventListener('click', () => {
+    const slogan = builderSloganTextArea.value.trim();
     if (slogan) {
         city.slogans.push(slogan);
     }
-    cityDesignerSloganTextArea.value = '';
-    cityDesignerSloganTextArea.focus();
+    builderSloganTextArea.value = '';
+    builderSloganTextArea.focus();
     displayCity();
 });
 
-cityDesignerSaveButton.addEventListener('click', () => {
+builderSaveButton.addEventListener('click', () => {
     // Need to deep copy the slogan array.
     cities.push(JSON.parse(JSON.stringify(city)));
 });
 
 // City Display Component
-const cityDisplaySection = document.querySelector('#city-display-section');
-const cityDisplayNameElement = cityDisplaySection.querySelector('p');
-const [cityDisplayBiomeImg, cityDisplayArchitectureImg] = cityDisplaySection.querySelectorAll('img');
-const cityDisplaySloganList = cityDisplaySection.querySelector('ul');
+const displaySection = document.querySelector('#display-section');
+const displayNameElement = displaySection.querySelector('p');
+const [displayBiomeImg, displayArchImg] = displaySection.querySelectorAll('img');
+const displaySloganList = displaySection.querySelector('ul');
 
 function displayCity() {
-    const { name, biome, architecture, slogans } = city;
+    const { name, biome, arch, slogans } = city;
 
-    cityDisplayNameElement.textContent = name;
-    cityDisplayBiomeImg.src = biome ? 'assets/city-parts/biome-' + biome + '.png' : '';
-    cityDisplayArchitectureImg.src = architecture ? 'assets/city-parts/architecture-' + architecture + '.png' : '';
+    displayNameElement.textContent = name;
+    displayBiomeImg.src = biome ? 'assets/city-parts/biome-' + biome + '.png' : '';
+    displayArchImg.src = arch ? 'assets/city-parts/architecture-' + arch + '.png' : '';
 
-    cityDisplaySloganList.innerHTML = '';
+    displaySloganList.innerHTML = '';
     for (const slogan of slogans) {
         const li = document.createElement('li');
         li.textContent = slogan;
-        cityDisplaySloganList.appendChild(li);
+        displaySloganList.appendChild(li);
     }
 }
 
